3e4ddb5ac509c63bc1eece43277213e5089b12e2
k1mlka <k1mlka@outlook.com>
2024-08-29 09:51:49 +0800

    RegQueryValueExA

--- fakereg/dllmain.cpp
+++ fakereg/dllmain.cpp
@@ -4,8 +4,9 @@
 #include "detours.h"
 #include <string>
 #include <sstream>
-
 #include <map>
+#include <tuple>
+
 #include <vector>
 
 #pragma region String Converter
@@ -25,10 +26,19 @@ static std::string WideStringToShiftJIS(const std::wstring& wideString) {
 }
 
 template<typename... Args>
+std::string OssFormatStringNormal(const Args&... args) {
+	std::ostringstream oss;
+	oss << "[fakereg] ";
+	(oss << ... << args);
+	return oss.str();
+}
+
+template<typename... Args>
 std::string OssFormatString(const Args&... args) {
 	std::ostringstream oss;
-	(oss << ... << args); //c++17
-	return oss.str();
+	((oss << args << " + "), ...); // c++17
+	std::string result = oss.str();
+	return result;
 }
 
 /// <summary>
@@ -45,20 +55,15 @@ static std::string ConvertShiftJISToGBK(const std::string& shiftJisStr) {
 	return gbkStr;
 }
 
-// TODO 1: 要达到打印调用前的函数参数 字符串或 byte，打印调用后函数参数 对比的效果。
-// for debug use
-//static void OutputDebugStringWithBytesA(LPCSTR str) {
-//	std::ostringstream oss;
-//	int i = 0;
-//
-//	while (str[i] != '\0') {  // 遍历直到遇到字符串结束符 '\0'
-//		oss << std::setw(2) << std::setfill('0') << std::hex << (unsigned int)(unsigned char)str[i] << " ";
-//		i++;
-//	}
-//
-//	std::string hexStr = oss.str();
-//	OutputDebugStringA(hexStr.c_str());
-//}
+void PrintBytesToDebug(LPBYTE lpData, DWORD dataSize) {
+	std::ostringstream oss;
+
+	for (DWORD i = 0; i < dataSize; ++i) {
+		oss << std::hex << std::uppercase << (int)lpData[i] << " ";
+	}
+
+	OutputDebugStringA(oss.str().c_str());
+}
 
 #pragma endregion
 
@@ -74,23 +79,82 @@ decltype(&RegEnumValueA) RealRegEnumValueA = RegEnumValueA;
 
 decltype(&RegOpenKeyA) RealRegOpenKeyA = RegOpenKeyA;
 
-// NOTE: fakereg.ini must be encode with UTF-16 LE 否则 GetPrivateProfileStringW 函数无法正确读取宽字符
+// NOTE:
+// 1. fakereg.ini must be encode with UTF-16 LE 否则 GetPrivateProfileStringW 函数无法正确读取宽字符
+// 2. ini 读取值文件夹路径末尾必须以 \ 结尾
+
 std::wstring iniFilePath;
 std::map<HKEY, std::string> fakeHKeyMap;
-HKEY fakeHKeyBase = reinterpret_cast<HKEY>(0xDEADBEEF);  // 用于生成虚假 hKey 的基值
+HKEY fakeHKeyBase = reinterpret_cast<HKEY>(0x00000470);  // 用于生成虚假 hKey 的基值
 HKEY currentFakeHKey = fakeHKeyBase;
 
 #pragma region Operator to Modify ini file
 
-static std::string AddQuoteString(std::string key)
+static std::string InsertWow6432NodePath(std::string& fullPath)
 {
-	return "\"" + std::move(key) + "\"";
+	const std::string insertStr = "WOW6432Node\\";
+
+	size_t firstSlashPos = fullPath.find('\\');
+	if (firstSlashPos != std::string::npos)
+	{
+		size_t secondSlashPos = fullPath.find('\\', firstSlashPos + 1);
+		if (secondSlashPos != std::string::npos)
+		{
+			// 在第二个反斜杠后插入 "WOW6432Node"
+			fullPath.insert(secondSlashPos + 1, insertStr);
+		}
+	}
+
+	return fullPath;
+}
+
+static bool IniSectionExists(std::string sectionName) {
+	wchar_t buffer[256];
+
+	auto fullPathu = ShiftJISToUTF16(sectionName);
+
+	// 尝试读取该 section 中的第一个 key。如果 section 不存在，则 buffer 中会返回默认值。
+	DWORD charsRead = GetPrivateProfileStringW(
+		fullPathu.c_str(), NULL, NULL, buffer, sizeof(buffer) / sizeof(wchar_t),
+		iniFilePath.c_str());
+
+	return charsRead > 0;
+}
+
+static std::string AddQuoteString(const std::string& key)
+{
+	return "\"" + key + "\"";
 }
 static std::wstring RemoveQuoteWString(std::wstring key)
 {
 	key.erase(0, 1);
 	key.pop_back();
 	return key;
+}
+
+static std::vector<BYTE> ParseHexData(const std::string& hexString) {
+	std::vector<BYTE> data;
+	size_t pos = 4; // 跳过 "hex:" 前缀
+
+	while (pos < hexString.size()) {
+		// 找到下一个逗号或字符串结束
+		size_t end = hexString.find(',', pos);
+		if (end == std::wstring::npos) {
+			end = hexString.size();
+		}
+
+		// 提取并转换当前的十六进制字符串部分
+		std::string byteString = hexString.substr(pos, end - pos);
+		BYTE byteValue = static_cast<BYTE>(std::strtol(byteString.c_str(), nullptr, 16));
+		data.push_back(byteValue);
+
+		pos = end + 1;  // 移动到下一个字符
+	}
+
+	return data;
+}
+DWORD ParseDwordData(const std::string& dwordString) {
+	return std::stoul(dwordString.substr(6), nullptr, 16); // Skip "dword:" and convert
 }
 static std::string NormalizeBackslashes(const std::string& input) {
 	std::string output;
@@ -109,33 +173,49 @@ static std::string NormalizeBackslashes(const std::string& input) {
 	return output;
 }
 
-static bool IniSectionExists(std::string sectionName) {
-	wchar_t buffer[256];
-
-	auto fullPathu = ShiftJISToUTF16(sectionName);
-
-	// 尝试读取该 section 中的第一个 key。如果 section 不存在，则 buffer 中会返回默认值。
-	DWORD charsRead = GetPrivateProfileStringW(
-		fullPathu.c_str(), NULL, NULL, buffer, sizeof(buffer) / sizeof(wchar_t),
-		iniFilePath.c_str());
-
-	return charsRead > 0;
-}
-
-static std::string ReadINIValue(std::string path, std::string key) {
+/// <summary>
+/// 交换 ini 与注册表中数据格式
+/// </summary>
+static DWORD ReadINIValue(std::string section, std::string key, LPBYTE lpData, LPDWORD lpcbData) {
 	wchar_t buffer[256];
 
 	auto keyQuote = AddQuoteString(key);
 
-	auto pathu = ShiftJISToUTF16(path);
+	auto sectionu = ShiftJISToUTF16(section);
 	auto keyu = ShiftJISToUTF16(keyQuote);
 
-	auto result = GetPrivateProfileStringW(pathu.c_str(), keyu.c_str(), L"", buffer, sizeof(buffer) / sizeof(wchar_t), iniFilePath.c_str());
+	auto result = GetPrivateProfileStringW(sectionu.c_str(), keyu.c_str(), L"", buffer, sizeof(buffer) / sizeof(wchar_t), iniFilePath.c_str());
 
-	auto shiftjis = WideStringToShiftJIS(buffer);
+	if (result == 0 && buffer[0] == L'\0')
+		return 0;
 
-	// TODO 2: 理应确定是字符串或者path后再处理，考虑type怎么处理
-	return NormalizeBackslashes(shiftjis);
+	auto encodeValue = WideStringToShiftJIS(buffer);
+
+	DWORD type = {};
+
+	if (encodeValue.find("hex:") == 0) {  // hex:00,00,00,00
+		type = REG_BINARY;
+
+		auto binaryData = ParseHexData(encodeValue);
+		memcpy(lpData, binaryData.data(), binaryData.size());
+		*lpcbData = binaryData.size();
+	}
+	else if (encodeValue.find("dword:") == 0) {  // dword:00000000
+		type = REG_DWORD;
+
+		DWORD dwordValue = ParseDwordData(encodeValue);
+		memcpy(lpData, &dwordValue, sizeof(DWORD));
+		*lpcbData = sizeof(DWORD);
+	}
+	else {
+		type = REG_SZ;
+
+		auto splashValue = NormalizeBackslashes(encodeValue);
+		memcpy(lpData, splashValue.c_str(), (splashValue.size() + 1));
+		*lpcbData = splashValue.size();
+	}
+
+	return type;
 }
 
 static std::string GetNthKeyValueInSection(const std::string& sectionName, int n) {
@@ -175,33 +255,34 @@ static std::string GetNthKeyValueInSection(const std::string& sectionName, int n
 #pragma endregion
 
 LONG WINAPI FakeRegOpenKeyExA(HKEY hKey, LPCSTR lpSubKey, DWORD ulOptions, REGSAM samDesired, PHKEY phkResult) {
-	std::string baseKey;
+	std::string fullPath;
 	if (hKey == HKEY_LOCAL_MACHINE) {
-		baseKey = "HKEY_LOCAL_MACHINE\\";
+		fullPath = "HKEY_LOCAL_MACHINE\\" + std::string(lpSubKey);
 	}
 	else if (hKey == HKEY_CURRENT_USER) {
-		baseKey = "HKEY_CURRENT_USER\\";
+		fullPath = "HKEY_CURRENT_USER\\" + std::string(lpSubKey);
 	}
 	else {
 		return RealRegOpenKeyExA(hKey, lpSubKey, ulOptions, samDesired, phkResult);
 	}
 
-	auto fullPath = baseKey + lpSubKey;
-
 	// read ini file key
-	if (!IniSectionExists(fullPath))
+	if (!IniSectionExists(fullPath) && !IniSectionExists(InsertWow6432NodePath(fullPath)))
 		return RealRegOpenKeyExA(hKey, lpSubKey, ulOptions, samDesired, phkResult);
+
+	// TEST Entry: The original behavior 取消注释以下来测试真实读取注册表的场景
+	/*OutputDebugStringA(OssFormatString("FakeRegOpenKeyExA  input", hKey, lpSubKey, ulOptions, samDesired, *phkResult).c_str());
+	auto result = RealRegOpenKeyExA(hKey, lpSubKey, ulOptions, samDesired, phkResult);
+	OutputDebugStringA(OssFormatString("FakeRegOpenKeyExA output", hKey, lpSubKey, ulOptions, samDesired, *phkResult, result).c_str());
+	fakeHKeyMap[*phkResult] = fullPath;
+	return result;*/
+	// TEST Entry END
 
 	HKEY fakeHKey = currentFakeHKey++;
 	fakeHKeyMap[fakeHKey] = fullPath;
 	*phkResult = fakeHKey;
 
-	// TEST: The original behavior 取消注释以下两行来测试真实读取注册表的情况
-	//auto result = RealRegOpenKeyExA(hKey, lpSubKey, ulOptions, samDesired, phkResult);
-	//fakeHKeyMap[*phkResult] = fullPath;
-
-	std::string message = OssFormatString("Open fake hKey ", *phkResult);
-	OutputDebugStringA(message.c_str());
+	OutputDebugStringA(OssFormatStringNormal("Open fake hKey: ", *phkResult).c_str());
 
 	return ERROR_SUCCESS;
 }
@@ -212,105 +293,45 @@ LONG WINAPI FakeRegCloseKey(HKEY hKey) {
 
 	fakeHKeyMap.erase(hKey);
 
-	std::string message = OssFormatString("Close hooked hKey: 0x", std::hex, std::uppercase, reinterpret_cast<uintptr_t>(hKey));
-	OutputDebugStringA(message.c_str());
+	OutputDebugStringA(OssFormatStringNormal("Close hooked hKey: 0x", std::hex, std::uppercase, reinterpret_cast<uintptr_t>(hKey)).c_str());
 	return ERROR_SUCCESS;
 }
 
-static std::vector<BYTE> ParseHexData(const std::string& hexString) {
-	std::vector<BYTE> data;
-	size_t pos = 4; // 跳过 "hex:" 前缀
-
-	while (pos < hexString.size()) {
-		// 找到下一个逗号或字符串结束
-		size_t end = hexString.find(',', pos);
-		if (end == std::wstring::npos) {
-			end = hexString.size();
-		}
-
-		// 提取并转换当前的十六进制字符串部分
-		std::string byteString = hexString.substr(pos, end - pos);
-		BYTE byteValue = static_cast<BYTE>(std::strtol(byteString.c_str(), nullptr, 16));
-		data.push_back(byteValue);
-
-		pos = end + 1;  // 移动到下一个字符
-	}
-
-	return data;
-}
-
-DWORD ParseDwordData(const std::string& dwordString) {
-	return std::stoul(dwordString.substr(6), nullptr, 16); // Skip "dword:" and convert
-}
-
+// LSTATUS RegQueryValueExA(
+//	[in]                HKEY    hKey,
+//	[in, optional]      LPCSTR  lpValueName,     // key
+//	                    LPDWORD lpReserved,
+//	[out, optional]     LPDWORD lpType,          // 传入设置为 NULL，表示不关心
+//	[out, optional]     LPBYTE  lpData,          // value 空白说明00
+//	[in, out, optional] LPDWORD lpcbData         // 接收值缓冲区大小，一般传入260，会在调用后调整为真实值，如 ".\" -> 3
+//);
 LONG WINAPI FakeRegQueryValueExA(HKEY hKey, LPCSTR lpValueName, LPDWORD lpReserved, LPDWORD lpType, LPBYTE lpData, LPDWORD lpcbData) {
-	auto it = fakeHKeyMap.find(hKey);
-	if (it == fakeHKeyMap.end())
+	if (fakeHKeyMap.find(hKey) == fakeHKeyMap.end())
 		return RealRegQueryValueExA(hKey, lpValueName, lpReserved, lpType, lpData, lpcbData);
 
-	// TEST: The original behavior
-	// Value before called
-	//auto result = RealRegQueryValueExA(hKey, lpValueName, lpReserved, lpType, lpData, lpcbData);
-	// Value after called
-	//OutputDebugStringA(ss.str().c_str());
-	//return result;
+	// Note: Must uncomment TEST Entry first
+	/*OutputDebugStringA(OssFormatString("FakeRegQueryValueExA  input", hKey, lpValueName, lpReserved, lpType, lpData, *lpcbData).c_str());
+	auto result = RealRegQueryValueExA(hKey, lpValueName, lpReserved, lpType, lpData, lpcbData);
+	OutputDebugStringA(OssFormatString("FakeRegQueryValueExA output", hKey, lpValueName, lpReserved, lpType, lpData, *lpcbData, result).c_str());
+	return result;*/
+	// Note END
 
-	std::string fullPath = it->second;
-	std::string ival = ReadINIValue(fullPath, std::string(lpValueName));
+	std::string encodeValue = {};
 
-	if (!ival.empty()) {
-		std::string message = OssFormatString("Query key: ", lpValueName, " result: ", ival.c_str());
-		OutputDebugStringA(message.c_str());
+	std::string fullPath = fakeHKeyMap.find(hKey)->second;
+	DWORD type = ReadINIValue(fullPath, std::string(lpValueName), lpData, lpcbData); // 不处理  ERROR_MORE_DATA.
+	// __KEY_NOT_FOUND__
+	if (type == 0)
+		return ERROR_FILE_NOT_FOUND;
 
-		// TODO 2: 考虑type怎么处理
-		if (ival.find("hex:") == 0) {  // 处理 REG_BINARY 数据 hex:00,00,00,00
-			auto binaryData = ParseHexData(ival);
-
-			if (lpData && lpcbData && *lpcbData >= binaryData.size()) {
-				memcpy(lpData, binaryData.data(), binaryData.size());
-				*lpcbData = binaryData.size();
-
-				if (lpType) {
-					*lpType = REG_BINARY;
-				}
-				return ERROR_SUCCESS;
-			}
-			else {
-				return ERROR_MORE_DATA;  // 缓冲区太小
-			}
-		}
-		if (ival.find("dword:") == 0) {  // 处理 REG_DWORD 数据 dword:00000000
-			DWORD dwordValue = ParseDwordData(ival);
-
-			if (lpData && lpcbData && *lpcbData >= sizeof(DWORD)) {
-				memcpy(lpData, &dwordValue, sizeof(DWORD));
-				*lpcbData = sizeof(DWORD);
-
-				if (lpType) {
-					*lpType = REG_DWORD;
-				}
-				return ERROR_SUCCESS;
-			}
-			else {
-				*lpcbData = sizeof(DWORD);
-				return ERROR_MORE_DATA;  // 缓冲区太小
-			}
-		}
-		else {  // 处理字符串数据
-			if (lpData && lpcbData && *lpcbData >= (ival.size() + 1)) {  // 注意这里不需要乘以 sizeof(wchar_t)，因为是 std::string
-				memcpy(lpData, ival.c_str(), (ival.size() + 1));  // 复制 std::string 数据，包括终止符
-				*lpcbData = (ival.size() + 1);
-
-				if (lpType) {
-					*lpType = REG_SZ;
-				}
-				return ERROR_SUCCESS;
-			}
-			else {
-				return ERROR_MORE_DATA;  // 缓冲区太小
-			}
-		}
+	if (lpType)
+	{
+		*lpType = type;
 	}
+
+	OutputDebugStringA(OssFormatStringNormal("Query key: ", lpValueName, " value: ", lpData).c_str());
+
+	return ERROR_SUCCESS;
 }
 
 // TODO 5:
